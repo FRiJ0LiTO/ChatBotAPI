b9f3c025bacbfcbf3d1c5c13c4cc4fe942631e0b
Jaime Cabrera <129076194+FRiJ0LiTO@users.noreply.github.com>
2024-05-28 01:00:48 -0600

    Update auth.py

--- auth.py
+++ auth.py
@@ -111,7 +111,7 @@ async def get_current_active_user(
 
 @router.post("/register")
 async def register_user(user: ModelUser = Body(...)):
-    return await create_user(user_data)
+    return await create_user(user)
 
 
 @router.post("/token")
